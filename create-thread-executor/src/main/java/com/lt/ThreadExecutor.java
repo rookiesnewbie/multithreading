9a9a864ece125b60054abb5822d130f50a50f17b
rookiesnewbie <2049448867@qq.com>
2023-09-09 13:21:56 +0800

    使用Executor创建线程

--- create-thread-executor/src/main/java/com/lt/ThreadExecutor.java
+++ create-thread-executor/src/main/java/com/lt/ThreadExecutor.java
@@ -1,7 +1,26 @@
 package com.lt;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadExecutor {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Thread.currentThread().getName()+ "我是主线程");
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+ "我是子线程");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName()+"子线程执行结束");
+            }
+        });
+
+        System.out.println(Thread.currentThread().getName()+"子线程执行结束");
     }
 }
