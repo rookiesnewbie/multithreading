30b22219c6e34dd2050d1ddd8b17c04d74781d88
rookiesnewbie <2049448867@qq.com>
2023-09-09 11:43:59 +0800

    使用lambda表达式创建线程，并开启线程

--- create-thread-lambda/src/main/java/com/lt/ThreadRunnable.java
+++ create-thread-lambda/src/main/java/com/lt/ThreadRunnable.java
@@ -2,6 +2,29 @@ package com.lt;
 
 public class ThreadRunnable {
     public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + "我是主线程");
 
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "我是子线程");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("子线程执行结束");
+        },"sun1").start();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "我是子线程");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("子线程执行结束");
+        },"sun2").start();
+
+
+        System.out.println("主线程执行完毕");
     }
 }
