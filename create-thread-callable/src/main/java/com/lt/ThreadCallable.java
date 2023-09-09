621282213eee481362bce06074607b8eb7177c57
rookiesnewbie <2049448867@qq.com>
2023-09-09 13:02:08 +0800

    使用Callable创建线程,并开启线程

--- create-thread-callable/src/main/java/com/lt/ThreadCallable.java
+++ create-thread-callable/src/main/java/com/lt/ThreadCallable.java
@@ -1,7 +1,26 @@
 package com.lt;
 
-public class ThreadCallable {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class ThreadCallable implements Callable<Integer> {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println(Thread.currentThread().getName()+"我是主线程");
+        ThreadCallable threadCallable = new ThreadCallable();
+        FutureTask<Integer> task = new FutureTask<Integer>(threadCallable);
+
+        new Thread(task).start();
+        Integer result = task.get();
+
+
+        System.out.println("子线程返回的数据："+ result);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName()+"我是子线程");
+        Thread.sleep(3000);
+        return 1;
     }
 }
