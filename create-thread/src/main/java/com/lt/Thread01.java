68a05221df7f8be26e44dabef827a3619317e9e4
rookiesnewbie <2049448867@qq.com>
2023-09-09 10:42:00 +0800

    通过进程Thread类重写run()方法创建线程

---
+++ create-thread/src/main/java/com/lt/Thread01.java
@@ -0,0 +1,28 @@
+package com.lt;
+
+public class Thread01 extends Thread {
+    /**
+     * 线程执行的代码就是在run方法中  执行结束 线程就死亡了
+     */
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "我是子线程");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread().getName());
+
+        //注意：启动线程是调用start()方法，而不是run()方法
+        new Thread01().start();
+
+        new Thread01().start();
+
+        //注意：调用start()线程并不是立即被cpu调度执行，而是由cpu调度执行决定
+        // 调用start()方法时 线程处于就状态 ----等待cpu调度
+        //
+        //调用run()方法时 线程从 就绪 到运行状态
+
+        //当run()方法 执行结束 线程就死亡了
+    }
+}
