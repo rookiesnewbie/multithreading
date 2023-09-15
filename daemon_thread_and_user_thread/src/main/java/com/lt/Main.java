65855b7ff925102c2001b9a87b86f267ecce284b
rookiesnewbie <2049448867@qq.com>
2023-09-15 23:00:30 +0800

    用户线程(独立存在的，不会因为其他用户线程退出而退出)

--- daemon_thread_and_user_thread/src/main/java/com/lt/Main.java
+++ daemon_thread_and_user_thread/src/main/java/com/lt/Main.java
@@ -2,6 +2,31 @@ package com.lt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("主线程开始执行");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(3000);
+                        System.out.println("我是子线程");
+
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+
+            }
+        });
+
+        thread.start();
+
+
+        System.out.println("主线程执行结束");
+
+
     }
 }
