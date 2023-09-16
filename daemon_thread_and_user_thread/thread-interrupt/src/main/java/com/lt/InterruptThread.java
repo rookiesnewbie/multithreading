59c8510724f734f62451b823a8ae41df2ddda623
rookiesnewbie <2049448867@qq.com>
2023-09-16 14:07:56 +0800

    Interrupt中断线程生效

---
+++ daemon_thread_and_user_thread/thread-interrupt/src/main/java/com/lt/InterruptThread.java
@@ -0,0 +1,31 @@
+package com.lt;
+
+public class InterruptThread extends Thread{
+    @Override
+    public void run() {
+        while (true){
+            System.out.println(1);
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(2);
+        }
+    }
+
+    public static void main(String[] args) {
+        InterruptThread interruptThread = new InterruptThread();
+        interruptThread.start();
+        System.out.println("主线程");
+
+        try {
+            Thread.sleep(3000);
+            System.out.println("子线程333");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        interruptThread.interrupt();
+    }
+}
