cde58a634fe55ded349940fffa22a7cf9eaceba8
lisircode <2246035400@qq.com>
2024-08-05 09:59:06 +0800

    可重入锁

---
+++ JUC/src/main/java/com/lt/locks/ReEntryLockDemo.java
@@ -0,0 +1,64 @@
+package com.lt.locks;
+
+public class ReEntryLockDemo {
+
+    public synchronized void m1(){
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+        m2();
+        System.out.println(Thread.currentThread().getName() + "\t end");
+    }
+
+    public synchronized void m2(){
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+        m3();
+    }
+
+    public synchronized void m3(){
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+    }
+
+
+
+    public static void main(String[] args) {
+
+        //ReEntryLockDemo.m11();
+        ReEntryLockDemo.m22();
+
+
+    }
+
+    public static void m11(){
+        final Object object = new Object();
+        new Thread(() ->{
+            synchronized (object){
+                System.out.println(Thread.currentThread().getName() +"\t 外层调用");
+                synchronized (object){
+                    System.out.println(Thread.currentThread().getName() +"\t 中层调用");
+                    synchronized (object){
+                        System.out.println(Thread.currentThread().getName() +"\t 内层调用");
+                    }
+                }
+            }
+        },"t1").start();
+
+        new Thread(() ->{
+            synchronized (object){
+                System.out.println(Thread.currentThread().getName() +"\t 外层调用");
+                synchronized (object){
+                    System.out.println(Thread.currentThread().getName() +"\t 中层调用");
+                    synchronized (object){
+                        System.out.println(Thread.currentThread().getName() +"\t 内层调用");
+                    }
+                }
+            }
+        },"t2").start();
+
+    }
+
+    public static void m22(){
+        ReEntryLockDemo reEntryLockDemo = new ReEntryLockDemo();
+        new Thread(() ->{
+            reEntryLockDemo.m1();
+        },"t1").start();
+    }
+}
