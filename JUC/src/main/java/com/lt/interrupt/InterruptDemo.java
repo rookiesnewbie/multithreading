042d5f6fbf7c657c1e3815acf42f51bf3f1df86a
lisircode <2246035400@qq.com>
2024-08-06 09:20:47 +0800

    通过一个AtomicBoolean变量实现 中断运行中的线程

--- JUC/src/main/java/com/lt/interrupt/InterruptDemo.java
+++ JUC/src/main/java/com/lt/interrupt/InterruptDemo.java
@@ -1,15 +1,21 @@
 package com.lt.interrupt;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class InterruptDemo {
 
-     static volatile boolean isStop = false;
+    static volatile boolean isStop = false;
+    static AtomicBoolean  atomicBoolean = new AtomicBoolean(false);
     public static void main(String[] args){
-        InterruptDemo.m1_volatile();
+        //InterruptDemo.m1_volatile();
+        InterruptDemo.m2_atomicBoolean();
 
     }
 
+    /**
+     * 通过一个volatile变量实现 中断运行中的线程
+     */
     public static void m1_volatile(){
         new Thread(() ->{
             while (true){
@@ -29,6 +35,32 @@ public class InterruptDemo {
 
         new Thread(() ->{
             isStop = true;  //停止线程
+        },"t2").start();
+    }
+
+
+    /**
+     * 通过一个atomicBoolean变量实现 中断运行中的线程
+     */
+    public static void m2_atomicBoolean(){
+        new Thread(() ->{
+            while (true){
+                if (atomicBoolean.get()){
+                    System.out.println(Thread.currentThread().isInterrupted() + "\t atomicBoolean被修改为true，程序停止");
+                    break;
+                }
+                System.out.println("--------------  hello atomicBoolean");
+            }
+        },"t1").start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() ->{
+            atomicBoolean.set(true);  //停止线程
         },"t2").start();
     }
 }
